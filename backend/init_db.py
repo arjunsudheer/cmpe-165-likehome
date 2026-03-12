8833621cfb4f052b81a78693788925020b8532ee
tofu <isabelleluu0w0@gmail.com>
2026-03-11 23:13:50 -0700

    fix: use begin(), check for population first

--- backend/init_db.py
+++ backend/init_db.py
@@ -1,13 +1,16 @@
 from db_connection import engine, Base
 from mock_hotels import mock_hotels, mock_hotel_rooms
+from sqlalchemy import select
+from models import Hotel
 import models
 
 def init_tables_and_data():
     Base.metadata.create_all(engine, checkfirst=True)
-    with engine.connect() as conn:
-        mock_hotels(conn)
-        mock_hotel_rooms(conn)
-        conn.commit()
+    with engine.begin() as conn:
+        hotel = conn.execute(select(Hotel)).first()
+        if hotel is None:
+            mock_hotels(conn)
+            mock_hotel_rooms(conn)
 
 if __name__ == "__main__":
     init_tables_and_data()
