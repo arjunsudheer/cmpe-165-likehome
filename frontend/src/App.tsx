c5e1e4f3c224d497ef4cc9839d81091228a8dfa8
iamgavtn <iamgavtn@gmail.com>
2026-03-19 14:20:18 -0700

    R08.2: Add booking conflict warning dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Register";
+import BookingConflictPage from "./BookingConflictWarning";
 
 export default function App() {
   return (
-    <div>
-      <Register />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/conflict" element={<BookingConflictPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}``
+}
