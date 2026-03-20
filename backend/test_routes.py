7eb13900d8db499931d5216416cfb674cd01ad84
tofu <isabelleluu0w0@gmail.com>
2026-03-20 02:16:48 -0700

    test: testing valid and invalid email registrations

--- backend/test_routes.py
+++ backend/test_routes.py
@@ -1,4 +1,34 @@
 class TestRegistration:
+
+    def test_accept_valid_registration(self, client):
+        response = client.post('/auth/register', json={'email': 'test@email.com', 'password': 'password', 'confirm_password': 'password'})
+        assert response.status_code == 201
+        assert response.get_json() == {"message": "user registered successfully"}
+
+    def test_reject_empty_email_field(self, client):
+        response = client.post('/auth/register', json={'email': '', 'password': 'password', 'confirm_password': 'password'})
+        assert response.status_code == 400
+        assert response.get_json() == {"error": "Email,password and confirm password are required"}
+
+    def test_reject_empty_password_field(self, client):
+        response = client.post('/auth/register', json={'email': 'test@email.com', 'password': '', 'confirm_password': ''})
+        assert response.status_code == 400
+        assert response.get_json() == {"error": "Email,password and confirm password are required"}
+
+    def test_reject_invalid_email_format(self, client):
+        response = client.post('/auth/register', json={'email': 'email.com', 'password': 'password', 'confirm_password': 'password'})
+        assert response.status_code == 400
+        assert response.get_json() == {"error": "Invalid email format"}
+    
+    def test_reject_non_matching_passwords(self, client):
+        response = client.post('/auth/register', json={'email': 'test@email.com', 'password': 'password', 'confirm_password': 'passwords'})
+        assert response.status_code == 400
+        assert response.get_json() == {"error": "Passwords do not match"}
+
+    def test_reject_under_minimum_password_length(self, client):
+        response = client.post('/auth/register', json={'email': 'test@email.com', 'password': 'pass', 'confirm_password': 'pass'})
+        assert response.status_code == 400
+        assert response.get_json() == {"error": "Password must be at least 6 characters"}
     
     def test_reject_duplicate_email(self, client):
         client.post('/auth/register', json={'email': 'duplicate@email.com', 'password': 'password', 'confirm_password': 'password'})
@@ -11,9 +41,3 @@ class TestRegistration:
         response = client.post('/auth/register', json={'email': 'Duplicate@email.com', 'password': 'password', 'confirm_password': 'password'})
         assert response.status_code == 409
         assert response.get_json() == {"message": "email already exists"}
-
-    def test_accept_non_duplicate_email(self, client):
-        client.post('/auth/register', json={'email': 'new@email.com', 'password': 'password', 'confirm_password': 'password'})
-        response = client.post('/auth/register', json={'email': 'newest@email.com', 'password': 'password', 'confirm_password': 'password'})
-        assert response.status_code == 201
-        assert response.get_json() == {"message": "user registered successfully"}
