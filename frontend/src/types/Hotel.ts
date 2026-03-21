508c59929e59ca9442b96b5d0d68009590a670a2
Timothy Kim <timothy.e.kim@sjsu.edu>
2026-03-20 22:28:18 -0700

    Initial commit of hotel filter component

---
+++ frontend/src/types/Hotel.ts
@@ -0,0 +1,11 @@
+export interface Hotel {
+  id: number;
+  name: string;
+  location: string;
+  rating: number;
+  description: string;
+  photos?: string[];
+
+  pricePerNight: number;
+  amenities: string[];
+}
